de4949e2e0d4d2d8e0bfc85154f4246377342779
Miccao <henriquedemicco@hotmail.com>
2021-11-02 12:22:54 -0300

    Primeiro commit do repo

---
+++ js/scrollReveal.js
@@ -0,0 +1,21 @@
+ScrollReveal().reveal('.l1', { delay: 500, easing: 'ease-in', duration: 800 });
+ScrollReveal().reveal('.l2', { delay: 600, easing: 'ease-in', duration: 800 });
+ScrollReveal().reveal('.l3', { delay: 700, easing: 'ease-in', duration: 800 });
+ScrollReveal().reveal('.l4', { delay: 800, easing: 'ease-in', duration: 800 });
+
+ScrollReveal().reveal('.mid-banner', { delay: 500, easing: 'ease-in', duration: 800 });
+
+ScrollReveal().reveal('#sec-categories', { delay: 500, easing: 'ease-in', duration: 800 });
+
+ScrollReveal().reveal('#sec-latest-posts h3', { delay: 500, easing: 'ease-in', duration: 800 });
+ScrollReveal().reveal('.pc1', { delay: 700, easing: 'ease-in', duration: 800 });
+ScrollReveal().reveal('.pc2', { delay: 900, easing: 'ease-in', duration: 800 });
+ScrollReveal().reveal('.pc3', { delay: 1000, easing: 'ease-in', duration: 800 });
+ScrollReveal().reveal('#sec-latest-posts .banner', { delay: 300, easing: 'ease-in', duration: 800 });
+
+
+ScrollReveal().reveal('#sec-partners h3', { delay: 500, easing: 'ease-in', duration: 800 });
+ScrollReveal().reveal('.pp1', { delay: 700, easing: 'ease-in', duration: 800 });
+ScrollReveal().reveal('.pp2', { delay: 900, easing: 'ease-in', duration: 800 });
+ScrollReveal().reveal('.pp3', { delay: 1000, easing: 'ease-in', duration: 800 });
+ScrollReveal().reveal('.pp4', { delay: 1200, easing: 'ease-in', duration: 800 });
